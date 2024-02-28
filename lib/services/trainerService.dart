5cfe7793c80caf96041b48bbf1b88ab188fdd9e2
AbhinandhV <abhinandh2255@gmail.com>
2024-02-28 16:20:58 +0530

    trainer front end

--- lib/services/trainerService.dart
+++ lib/services/trainerService.dart
@@ -1,40 +1,33 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
-import 'package:fitgym/models/trainerModel.dart';
-
-class TrainerApiService{
-
-  Future<List<Trainer>> getTrainer()async{
-    var client = http.Client();
-    var apiUrl =Uri.parse("http://localhost:3001/api/trainer/viewtrainers");
-    var response=await client.get(apiUrl);
-    if(response.statusCode==200){
-      return trainerFromJson(response.body);
-    }
-    else{
-      return [];
-    }
-  }
-Future<dynamic>sendData(String name,String age,String emailid,String password)async{
-    var client=http.Client();
-    var apiUrl=Uri.parse("http://localhost:3001/api/trainer/addtrainer");
+class Trainer
+{
+  Future<dynamic> TrainerLogin(
+      String email,String password
+      )async
+  {
+    var client= http.Client();
+    var apiUrl=Uri.parse("http://localhost:3001/api/trainer/signintrainer");
     var response=await client.post(apiUrl,
-    headers: <String,String>{
-      "Content-Type":"application/json;charset=UTF-8"
+    headers: <String,String>
+    {
+      "Content-Type":"application/json; charset=UTF-8"
     },
-    body: jsonEncode(<String,String>{
-      "name": name,
-      "age": age,
-      "emailid": emailid,
-      "password": password,
-    })
+      body: jsonEncode(<String,String>
+      {
+        "emailid":email,
+        "password":password
+      }
+      )
     );
-    if(response.statusCode==200){
-      return json.decode(response.body);
-    }
-    else{
-      throw Exception("Failure to add");
-    }
-}
-
+    if(response.statusCode==200)
+      {
+        return json.decode(response.body);
+      }
+    else
+      {
+        throw Exception("failed to load data");
+      }
+  }
 }
