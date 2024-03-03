f777fc0d3a1c43d2027a59a0aa40c3060a538381
harifa123 <harifarifu@gmail.com>
2024-03-03 19:48:35 +0530

    new branch

--- lib/pages/deleteMember.dart
+++ lib/pages/deleteMember.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../models/userViewModel.dart';
+import '../services/memberViewService.dart';
+import '../services/userService.dart';
 
 class deleteMember extends StatefulWidget {
   const deleteMember({super.key});
@@ -8,46 +11,106 @@ class deleteMember extends StatefulWidget {
 }
 
 class _deleteMemberState extends State<deleteMember> {
+  Future<List<Viewws >> ? data;
+
+  @override
+  void initState() {
+    super.initState();
+    data = MemberViewApiService().getViews(); // Modified to include user IDs
+  }
+
+  void deleteUserApi(String id) async {
+    final response = await UserServiceApi().deleteData(id);
+    if (response["status"] == "success") {
+      showSimplePopup1(context, "User Deleted!");
+      setState(() {
+        data = MemberViewApiService().getViews(); // Refresh user data after deletion
+      });
+    } else {
+      print("Error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.white,
           leading: IconButton(style: IconButton.styleFrom(
-              foregroundColor: Colors.white
+              foregroundColor: Colors.black
           ),
               onPressed: (){
                 Navigator.pop(context);
               }, icon: Icon(Icons.arrow_back)),
-          backgroundColor: Colors.blue,
-          title: Text("Delete Member",style: TextStyle(color: Colors.white),),
+          title: Text("Delete Members",style: TextStyle(color: Colors.black),),
         ),
-        body: ListView(
+        body: Container(
+          height: 1000,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.black38, Colors.white, Colors.black38]),
+          ),
           padding: EdgeInsets.all(20),
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                  labelText: "Name",
-                  border: OutlineInputBorder()
-              ),
-            ),
-            SizedBox(height: 10,),
-            SizedBox(height: 50,
-                width: 200,
-                child: ElevatedButton(style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)
-                    )
-                ),
-                    onPressed: (){
-                    }, child: Text("Delete"))
-            ),
-          ],
+          child: FutureBuilder(future: data, builder: (context,snapshot)
+          {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(child: Text('No users found.'));
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (value,index)
+                  {
+                    return SingleChildScrollView(
+                      child: Card(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 15,
+                            ),
+                            ListTile(
+                              title: Text("Name: "+snapshot.data![index].name.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+                              subtitle: Text("Email: "+snapshot.data![index].email.toString()+ "\n" +"Package Name: "+snapshot.data![index].packageName.toString()+ "\n" +"Due Amount: "+snapshot.data![index].dueAmount.toString()+ "\n" +"Remaining Days For Due: "+snapshot.data![index].remainingDaysForNextDue.toString()),
+                              leading: CircleAvatar(
+                                child: Text(snapshot.data![index].name[0]),
+                              ),
+                              trailing: ElevatedButton(style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black12,
+                                  foregroundColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20)
+                                  )
+                              ),
+                                  onPressed: ()
+                                  {
+                                    deleteUserApi(snapshot.data![index].id);
+                                  } ,
+                                  child: Text("Delete")),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            }
+          }),
         ),
       ),
     );
+  }
+  void showSimplePopup1(BuildContext context, String s) {
+    final snackBar = SnackBar(
+      content: Text(s,style: TextStyle(color: Colors.black),),
+      duration: Duration(seconds: 3),
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: Colors.white70,
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
